7c14f3ff5d288b0a236be141218ac62bb1818624
lalmeida <leonardo.fig.almeida@gmail.com>
2022-09-08 20:05:20 -0300

    Adding feature (with associated role) to change cap

--- contracts/ERC20FancyToken.sol
+++ contracts/ERC20FancyToken.sol
@@ -10,6 +10,8 @@ import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol"
  */
 contract ERC20FancyToken is ERC20PresetMinterPauser {
 
+    bytes32 public constant CAP_CHANGER_ROLE = keccak256("CAP_CHANGER_ROLE");
+
     uint256 private _cap;
 
     /**
@@ -23,6 +25,9 @@ contract ERC20FancyToken is ERC20PresetMinterPauser {
      */
     constructor(string memory name, string memory symbol, uint256 cap_) ERC20PresetMinterPauser (name, symbol) {
         require(cap_ > 0, "ERC20Capped: cap is 0");
+        
+        _setupRole(CAP_CHANGER_ROLE, _msgSender());
+
         _cap = cap_;
     }
 
@@ -31,6 +36,17 @@ contract ERC20FancyToken is ERC20PresetMinterPauser {
      * @dev Returns the cap on the token's total supply.
      */
     function cap() public view virtual returns (uint256) {
+        return _cap;
+    }
+
+
+    /**
+     * @dev Returns the cap on the token's total supply.
+     */
+    function change_cap(uint256 new_cap) public returns (uint256) {
+        require(hasRole(CAP_CHANGER_ROLE, _msgSender()), "ERC20FancyToken: must have cap changer role to change cap");
+        require(new_cap >= totalSupply(), "New cap has to be greater or equal to the current total supply." );
+        _cap = new_cap;
         return _cap;
     }
 
